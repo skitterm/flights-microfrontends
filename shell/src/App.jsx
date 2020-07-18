c36fa9bb1cff649c9c91a19095589275d4b59ae3
skitterm <skitterman@esri.com>
2020-07-17 18:25:50 -0700

    Shell can now load 2 remotes without console error

--- shell/src/App.jsx
+++ shell/src/App.jsx
@@ -2,6 +2,22 @@ import React, { useState, Suspense } from "react";
 import ReactDOM from "react-dom";
 import styled from "styled-components";
 
+// loadComponent comes from module-federation-examples repo:
+// https://github.com/module-federation/module-federation-examples/blob/bae09de0a68327f411f733d0fe84c9534770afab/advanced-api/dynamic-remotes/app1/src/App.js
+function loadComponent(scope, module) {
+  return async () => {
+    // Initializes the share scope. This fills it with known provided modules from this build and all remotes
+    await __webpack_init_sharing__("default");
+
+    const container = window[scope]; // or get the container somewhere else
+    // Initialize the container, it may provide shared modules
+    await container.init(__webpack_share_scopes__.default);
+    const factory = await window[scope].get(module);
+    const Module = factory();
+    return Module;
+  };
+}
+
 const Main = styled.div`
   margin: auto;
   max-width: 900px;
@@ -19,51 +35,31 @@ const App = () => {
     setIsHeaderReady(true);
   };
 
-  const [isViewFlightsReady, setIsViewFlightsReady] = useState(true);
+  const [isViewFlightsReady, setIsViewFlightsReady] = useState(false);
 
-  // const script2 = document.createElement("script");
-  // script2.type = "text/javascript";
-  // script2.src = `${VIEW_FLIGHTS_URL}/remoteEntry.js`;
-  // document.head.appendChild(script2);
+  const script2 = document.createElement("script");
+  script2.type = "text/javascript";
+  script2.src = `${VIEW_FLIGHTS_URL}/remoteEntry.js`;
+  document.head.appendChild(script2);
 
-  // script2.onload = () => {
-  //   setIsViewFlightsReady(true);
-  // };
+  script2.onload = () => {
+    setIsViewFlightsReady(true);
+  };
 
   if (!isHeaderReady || !isViewFlightsReady) {
     return <h1>Loading</h1>;
   }
 
   const Header = React.lazy(loadComponent("header", "./Header"));
-
-  // loadComponent comes from module-federation-examples repo:
-  // https://github.com/module-federation/module-federation-examples/blob/bae09de0a68327f411f733d0fe84c9534770afab/advanced-api/dynamic-remotes/app1/src/App.js
-  function loadComponent(scope, module) {
-    return async () => {
-      // Initializes the share scope. This fills it with known provided modules from this build and all remotes
-      await __webpack_init_sharing__("default");
-
-      const container = window[scope]; // or get the container somewhere else
-      // Initialize the container, it may provide shared modules
-      await container.init(__webpack_share_scopes__.default);
-      const factory = await window[scope].get(module);
-      const Module = factory();
-      return Module;
-    };
-  }
-
-  // const ViewFlights = React.lazy(() =>
-  //   window["viewFlights"].get("./ViewFlights").then((factory) => {
-  //     const module = factory();
-  //     return module;
-  //   })
-  // );
+  const ViewFlights = React.lazy(loadComponent("viewFlights", "./ViewFlights"));
 
   return (
     <Suspense fallback="Falling back">
       <div>
         <Header />
-        <Main>{/* <ViewFlights /> */}</Main>
+        <Main>
+          <ViewFlights />
+        </Main>
       </div>
     </Suspense>
   );
