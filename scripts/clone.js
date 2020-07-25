35c88a4e6ac1c184cde9b2a3c29e68ec278a6ad0
skitterm <skitterman@esri.com>
2020-07-25 13:48:00 -0700

    Checks out repo at specific commit

--- scripts/clone.js
+++ scripts/clone.js
@@ -1,41 +1,70 @@
 const { rmdir, mkdir } = require("fs");
 const { exec } = require("child_process");
 
-const directory = process.argv[2];
-if (!directory) {
-  throw new Error("Please provide a directory");
-}
+try {
+  const directory = process.argv[2];
+  if (!directory) {
+    throw new Error("Please provide a directory");
+  }
 
-rmdir("./copy", () => {
-  mkdir("./copy", () => {
-    exec(
-      "git clone https://github.com/skitterm/flights-microfrontends.git",
-      {
-        cwd: "./copy",
-      },
-      () => {
-        console.log("DONE CLONING");
+  const tag = process.argv[3];
+  if (!tag) {
+    throw new Error("Please provide a tag or commit hash");
+  }
 
-        exec(
-          "yarn",
-          {
-            cwd: `./copy/flights-microfrontends/${directory}`,
-          },
-          () => {
-            console.log(`DONE INSTALLING DEPENDENCIES TO ${directory}`);
+  rmdir("./../copy", () => {
+    mkdir("./../copy", () => {
+      exec(
+        "git clone https://github.com/skitterm/flights-microfrontends.git",
+        {
+          cwd: "./../copy",
+        },
+        () => {
+          console.log("DONE CLONING");
 
-            exec(
-              "yarn deploy",
-              {
-                cwd: `./copy/flights-microfrontends/${directory}`,
-              },
-              () => {
-                console.log(`DONE DEPLOYING TO ${directory}`);
-              }
-            );
-          }
-        );
-      }
-    );
+          exec(
+            `git checkout ${tag}`,
+            {
+              cwd: "./../copy/flights-microfrontends",
+            },
+            () => {
+              console.log(`DONE CHECKING OUT TAG/COMMIT ${tag}`);
+
+              exec(
+                "yarn",
+                {
+                  cwd: `./../copy/flights-microfrontends`,
+                },
+                () => {
+                  console.log(`DONE INSTALLING GENERAL DEPENDENCIES`);
+
+                  exec(
+                    "yarn",
+                    {
+                      cwd: `./../copy/flights-microfrontends/${directory}`,
+                    },
+                    () => {
+                      console.log(`DONE INSTALLING DIRECTORY DEPENDENCIES`);
+
+                      exec(
+                        "yarn deploy",
+                        {
+                          cwd: `./../copy/flights-microfrontends/${directory}`,
+                        },
+                        () => {
+                          console.log(`DONE DEPLOYING TO ${directory}`);
+                        }
+                      );
+                    }
+                  );
+                }
+              );
+            }
+          );
+        }
+      );
+    });
   });
-});
+} catch (err) {
+  console.log(err);
+}
