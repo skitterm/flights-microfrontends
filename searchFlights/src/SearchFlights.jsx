e7494cbd638fd427fd1386514c11a09222e6ba2e
skitterm <skitterman@esri.com>
2020-08-08 10:59:21 -0700

    Inputs for origin, destination city search with those parameters

--- searchFlights/src/SearchFlights.jsx
+++ searchFlights/src/SearchFlights.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import designSystem from "design/design";
+import Button from "design/Button";
 import LoadingIndicator from "./LoadingIndicator";
 import SearchResults from "./SearchResults";
 
@@ -8,32 +9,56 @@ const Heading = styled.h1`
   font-size: ${designSystem.fontSize.display};
 `;
 
+const Form = styled.div`
+  display: flex;
+  align-items: stretch;
+`;
+
+const Input = styled.input`
+  font-size: ${designSystem.fontSize.md};
+  margin-right: ${designSystem.spacing.sm};
+`;
+
 export default () => {
   const [flights, setFlights] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [originCity, setOriginCity] = useState("");
+  const [destinationCity, setDestinationCity] = useState("");
 
-  useEffect(() => {
-    const fetchFlights = async () => {
-      try {
-        // params are depcity and arrcity
-        const response = await fetch(
-          "http://18.191.215.81:33349/searchflights"
-        );
-        const json = await response.json();
-        setFlights(json);
-      } catch (err) {
-        console.log("error: ", err);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+  const onOriginInputChange = (event) => {
+    setOriginCity(event.target.value);
+  };
 
-    fetchFlights();
-  }, []);
+  const onDestinationInputChange = (event) => {
+    setDestinationCity(event.target.value);
+  };
+
+  const fetchFlights = async () => {
+    try {
+      const response = await fetch(
+        `http://18.191.215.81:33349/searchflights?depcity=${originCity}&arrcity=${destinationCity}`
+      );
+      const json = await response.json();
+      setFlights(json);
+    } catch (err) {
+      console.log("error: ", err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
       <Heading>Search Flights</Heading>
+      <Form>
+        <Input type="text" placeholder="Origin" onInput={onOriginInputChange} />
+        <Input
+          type="text"
+          placeholder="Destination"
+          onInput={onDestinationInputChange}
+        />
+        <Button onClick={fetchFlights}>Search</Button>
+      </Form>
       {isLoading ? <LoadingIndicator /> : <SearchResults flights={flights} />}
     </>
   );
